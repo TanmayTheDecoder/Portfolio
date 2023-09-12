0fb457869d7cf53d43a85d58dc0c390f45d03245
Tanmay Dobariya <dobariyatanmay@gmail.com>
2023-09-12 18:54:54 +0530

    portfolio

--- src/components/SideList.jsx
+++ src/components/SideList.jsx
@@ -8,7 +8,7 @@ const SideList = () => {
     <div>
       <div className="side-list-container w-100 h-90p">
         <ul className='side-list h-90 w-100 flex-column-even'>
-            <li className='flex-centered'><Link to='/portfolio/' className='flex'><Unicons.UilEstate color ='var(--pale-blue)' size ='18' />&nbsp;Home</Link></li>
+            <li className='flex-centered'><Link to='/' className='flex'><Unicons.UilEstate color ='var(--pale-blue)' size ='18' />&nbsp;Home</Link></li>
             <li className='flex-centered'><Link to='/about' className='flex'><Unicons.UilUserExclamation color ='var(--pale-blue)' size ='20'/>&nbsp;About</Link></li>
             <li className='flex-centered'><Link to='/experience' className='flex'><Unicons.UilTrophy color ='var(--pale-blue)' size ='18'/>&nbsp;Experience</Link></li>
             <li className='flex-centered'><Link to='/contact' className='flex'><Unicons.UilPhoneAlt color ='var(--pale-blue)' size ='18'/>&nbsp;Contact</Link></li>
