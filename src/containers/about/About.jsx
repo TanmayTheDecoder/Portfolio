31458ed8ef6d8437ca41f440f6bf8564f6b15346
TanmayTheDecoder <dobariyatanmay@gmail.com>
2024-09-21 17:33:18 +0530

    modified some styles and updated resume

--- src/containers/about/About.jsx
+++ src/containers/about/About.jsx
@@ -1,56 +1,66 @@
-import React from "react";
-import "./about.css";
-import CommonBackground from "../../components/CommonBackground";
-import Cubes from "../../components/Cubes";
+import React from 'react';
+import './about.css';
+import CommonBackground from '../../components/CommonBackground';
+import Cubes from '../../components/Cubes';
 const About = () => {
-  const ResumeData = {
-    filePath : require('../about/Resume.pdf'),
-    alt : 'file not found'
-  }
+	const ResumeData = {
+		filePath: require('../about/Resume.pdf'),
+		alt: 'file not found',
+	};
 
-  const aboutBtnFunction = (e) => {
-    const aboutBtn = document.getElementById('about-btn');
-    return function () {
-      aboutBtn.addEventListener('click', () => {
-        e.preventDefault();
-      })
-    }
-  }
-  return (
-    <div className="about-container w-100">
-      <CommonBackground />
-      <div className="about-inner w-100 h-100p flex-centered">
-        <div className="about-contents w-80">
-          <div className="about-contents-inner w-100 flex-column-space">
-            <div className="about-title w-100 flex-column-start">
-              Glance inside me...
-            </div>
-            <div className="about-description flex-column-start">
-              Hi, I'm Tanmay , a MERN stack web developer. I'm proficient in
-              React js, HTML 5, CSS 3, JavaScript, Bootstrap, Express js, Node js, MongoDB, C,
-              C++. I'm passionate about building great web applications that
-              solve real-world problems. I'm a hard worker and am always willing
-              to go the extra mile. I'm also a great communicator and am able to
-              work effectively with both clients and team members. If you're
-              looking for a talented and experienced MERN stack web developer,
-              I'm the perfect candidate. I'm confident that I can use my skills
-              and experience to make a positive impact on the world.
-              <div className="download-cv">
-                <button className="btn enlarge" onClick={aboutBtnFunction} id="about-btn">
-                  <a href={ResumeData.filePath} download>
-                    Download Resume
-                  </a>
-                </button>
-              </div>
-            </div>
-            <div className="development-cubes w-100">
-              <Cubes />
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	const aboutBtnFunction = (e) => {
+		const aboutBtn = document.getElementById('about-btn');
+		return function () {
+			aboutBtn.addEventListener('click', () => {
+				e.preventDefault();
+			});
+		};
+	};
+	return (
+		<div className='about-container w-100'>
+			<CommonBackground />
+			<div className='about-inner w-100 h-100p flex-centered'>
+				<div className='about-contents w-80'>
+					<div className='about-contents-inner w-100 flex-column-space'>
+						<div className='about-title w-100 flex-column-start'>
+							Glance inside me...
+						</div>
+						<div className='about-description flex-column-start'>
+							I am a MERN Stack Web Developer skilled in a wide range of
+							technologies, including React.js, Next.js, web sockets, Three.js,
+							React Spring, Framer Motion, Vite.js, HTML5, CSS3, and Bootstrap.
+							I am proficient in MongoDB, Express.js, Node.js, and have
+							extensive experience with Redux (Core Redux, Redux Thunk, Redux
+							Saga, Redux Toolkit). I am adept at creating and managing CI/CD
+							pipelines, working with SQL databases, and have familiarity with
+							Angular. Additionally, I have knowledge of blockchain technology,
+							smart contracts, and crypto integration. I have worked on various
+							projects, including Payu365 (payment interface, admin site, and
+							CMS), DigiChatHub (chatting app), and Expensify (expense tracking
+							app), all utilizing technologies like Next.js and Redux.
+							<div className='download-cv'>
+								<button
+									className='btn enlarge'
+									onClick={aboutBtnFunction}
+									id='about-btn'
+								>
+									<a
+										href={ResumeData.filePath}
+										download
+									>
+										Download Resume
+									</a>
+								</button>
+							</div>
+						</div>
+						<div className='development-cubes w-100'>
+							<Cubes />
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default About;
--- src/containers/experience/Experience.jsx
+++ src/containers/experience/Experience.jsx
@@ -1,186 +1,160 @@
-import React from "react";
-import "./experience.css";
-import CommonBackground from "../../components/CommonBackground";
+import React from 'react';
+import './experience.css';
+import CommonBackground from '../../components/CommonBackground';
 const Experience = () => {
-  const cardFunction = () => {
-    const linkBox = document.getElementsByClassName("link-box");
-      return function (e) {
-        linkBox.foreach((_,i) => {
-          e.preventDefault(linkBox[i]);
-        })
-      } 
-  };
+	const cardFunction = () => {
+		const linkBox = document.getElementsByClassName('link-box');
+		return function (e) {
+			linkBox.foreach((_, i) => {
+				e.preventDefault(linkBox[i]);
+			});
+		};
+	};
 
-  return (
-    <>
-      <div className="experience-container w-100 ">
-        <CommonBackground />
-        <div className="experience-inner w-100  flex-centered">
-          <div className="experience-contents w-80">
-            <div className="experience-title w-100 flex-column-start">
-              Dive into my works...
-            </div>
-            <div className="cards-row">
-              <div className="cards w-100 flex-column-start">
-                <div className="card h-40 w-30">
-                  <a
-                    href="https://github.com/TanmayTheDecoder/Way-To-Code.git"
-                    target="blank"
-                  >
-                    <div className="link-box adjust" onClick={cardFunction}>
-                      <h3>Way To Code</h3>
-                      <p>
-                        This is a Group project in which I've contributed and
-                        redesigned the whole website using HTML,CSS &
-                        Javascript.
-                      </p>
-                    </div>
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div className="cards-row">
-              <div className="cards w-100 flex-column-end">
-                <div className="card h-40 w-30">
-                  <a
-                    href="https://github.com/TanmayTheDecoder/Nicepage.git"
-                    target="blank"
-                  >
-                    <div className="link-box adjust" onClick={cardFunction}>
-                      <h3>Nice Page</h3>
-                      <p>
-                        This web page has some animations. It's designed using
-                        HTML & CSS.
-                      </p>
-                    </div>
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="experience-container w-100">
-        <CommonBackground />
-        <div className="experience-inner w-100  flex-centered">
-          <div className="experience-contents w-80">
-            <div className="cards-row add-margin">
-              <div className="cards w-100 flex-column-start">
-                <div className="card h-40 w-30">
-                  <a
-                    href="https://github.com/TanmayTheDecoder/Homepage-Cool-.git"
-                    target="blank"
-                  >
-                    <div className="link-box adjust" onClick={cardFunction}>
-                      <h3>Cool Home Page</h3>
-                      <p>
-                        This is a simple Home Page based on cool theme designed
-                        using HTML & CSS.
-                      </p>
-                    </div>
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div className="cards-row add-margin">
-              <div className="cards w-100 flex-column-end">
-                <div className="card h-40 w-30">
-                  <a
-                    href="https://github.com/TanmayTheDecoder/tic_tac_toe.git"
-                    target="blank"
-                  >
-                    <div className="link-box adjust" onClick={cardFunction}>
-                      <h3>Tic Tac Toe Game</h3>
-                      <p>I Made this game using Reactjs.</p>
-                    </div>
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="experience-container w-100">
-        <CommonBackground />
-        <div className="experience-inner w-100  flex-centered">
-          <div className="experience-contents w-80">
-            <div className="cards-row add-margin">
-              <div className="cards w-100 flex-column-start">
-                <div className="card h-40 w-30">
-                  <a
-                    href="https://github.com/TanmayTheDecoder/HTML-Template.git"
-                    target="blank"
-                  >
-                    <div className="link-box adjust" onClick={cardFunction}>
-                      <h3>Anita Dongre Website</h3>
-                      <p>
-                        This Template made using HTML & CSS and it's fully
-                        responsive.
-                      </p>
-                    </div>
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div className="cards-row add-margin">
-              <div className="cards w-100 flex-column-end">
-                <div className="card h-40 w-30">
-                  <a
-                    href="https://github.com/TanmayTheDecoder/eleconone.git"
-                    target="blank"
-                  >
-                    <div className="link-box adjust" onClick={cardFunction}>
-                      <h3>Eleconone</h3>
-                      <p>This Website is made using Reactjs.(Responsive Work is going on.)</p>
-                    </div>
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="experience-container w-100">
-        <CommonBackground />
-        <div className="experience-inner w-100  flex-centered">
-          <div className="experience-contents w-80">
-            <div className="cards-row add-margin">
-              <div className="cards w-100 flex-column-start">
-                <div className="card h-40 w-30">
-                  <a
-                    href="https://github.com/TanmayTheDecoder/react_template.git"
-                    target="blank"
-                  >
-                    <div className="link-box adjust" onClick={cardFunction}>
-                      <h3>Creators</h3>
-                      <p>
-                        This Template is made using Reactjs.
-                      </p>
-                    </div>
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div className="cards-row add-margin">
-              <div className="cards w-100 flex-column-end">
-                <div className="card h-40 w-30">
-                  <a
-                    href="https://github.com/TanmayTheDecoder/api_handling.git"
-                    target="blank"
-                  >
-                    <div className="link-box adjust" onClick={cardFunction}>
-                      <h3>Eleconone</h3>
-                      <p>Handled the data provided by API and made a website with it.</p>
-                    </div>
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className='experience-container w-100 '>
+				<CommonBackground />
+				<div className='experience-inner w-100  flex-centered'>
+					<div className='experience-contents w-80'>
+						<div className='experience-title w-100 flex-column-start'>
+							Dive into my works...
+						</div>
+						<div className='cards-row'>
+							<div className='cards w-100 flex-column-start'>
+								<div className='card h-40 w-30'>
+									<a
+										href='https://payu365.vercel.app/'
+										target='blank'
+									>
+										<div
+											className='link-box adjust'
+											onClick={cardFunction}
+										>
+											<h3>PayU365</h3>
+											<p>Payment interface made with Next js and redux.</p>
+										</div>
+									</a>
+								</div>
+							</div>
+						</div>
+						<div className='cards-row'>
+							<div className='cards w-100 flex-column-end'>
+								<div className='card h-40 w-30'>
+									<a
+										href='https://payu365admin.vercel.app/'
+										target='blank'
+									>
+										<div
+											className='link-box adjust'
+											onClick={cardFunction}
+										>
+											<h3>PayU365 Admin</h3>
+											<p>
+												Admin site for payu365 made with Next js and redux .
+											</p>
+										</div>
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className='experience-container w-100'>
+				<CommonBackground />
+				<div className='experience-inner w-100  flex-centered'>
+					<div className='experience-contents w-80'>
+						<div className='cards-row add-margin'>
+							<div className='cards w-100 flex-column-start'>
+								<div className='card h-40 w-30'>
+									<a
+										href='https://payu365cms.vercel.app/'
+										target='blank'
+									>
+										<div
+											className='link-box adjust'
+											onClick={cardFunction}
+										>
+											<h3>PayU365 CMS</h3>
+											<p>
+												Content management system for payu365 made with Next js
+												and redux .
+											</p>
+										</div>
+									</a>
+								</div>
+							</div>
+						</div>
+						<div className='cards-row add-margin'>
+							<div className='cards w-100 flex-column-end'>
+								<div className='card h-40 w-30'>
+									<a
+										href='https://payu365blockchain.vercel.app/'
+										target='blank'
+									>
+										<div
+											className='link-box adjust'
+											onClick={cardFunction}
+										>
+											<h3>PayU Blockchain</h3>
+											<p>
+												Next js app with web 3, wagmi, viem, Metamask & coin
+												gecko api integration
+											</p>
+										</div>
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className='experience-container w-100'>
+				<CommonBackground />
+				<div className='experience-inner w-100  flex-centered'>
+					<div className='experience-contents w-80'>
+						<div className='cards-row add-margin'>
+							<div className='cards w-100 flex-column-start'>
+								<div className='card h-40 w-30'>
+									<a
+										href='https://dev.d24g9k0ba6iut7.amplifyapp.com/'
+										target='blank'
+									>
+										<div
+											className='link-box adjust'
+											onClick={cardFunction}
+										>
+											<h3>Expensify</h3>
+											<p>Expense tracking app made with Next js and redux.</p>
+										</div>
+									</a>
+								</div>
+							</div>
+						</div>
+						<div className='cards-row add-margin'>
+							<div className='cards w-100 flex-column-end'>
+								<div className='card h-40 w-30'>
+									<a
+										href='https://dev.d3usebntg3hnpw.amplifyapp.com/'
+										target='blank'
+									>
+										<div
+											className='link-box adjust'
+											onClick={cardFunction}
+										>
+											<h3>Digichat Hub</h3>
+											<p>Chatting app made with Next js and redux</p>
+										</div>
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default Experience;
